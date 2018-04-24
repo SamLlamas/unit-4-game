2af7217a2d6c930e7ab5f479c09b0f175c1b7710
Sam Llamas <sam.llamas@utexas.edu>
2018-04-23 23:59:07 -0500

    UI update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,52 @@
-var scoreToGuess = 0;
-var myNumber = 0;
-var losses = 0;
-var wins = 0;
-var won = false;
 
 
 $(document).ready(function() {
+    var myNumber = 0;
+    var loses = 0;
+    var wins = 0;
+    $("#guess").text(String(Math.floor(Math.random() * 102) + 19));
+    var scoreToGuess = parseInt($("#guess").text());
 
-    while (won === false){
-        
+    function initializeGame(){
+        $("#guess").text(String(Math.floor(Math.random() * 102) + 19));
+        scoreToGuess = parseInt($("#guess").text());
+        myNumber = 0;
+        for (i = 1; i<5;i++){
+            $("#crystal"+String(i)).val(Math.floor(Math.random() * 12) + 1);
+            console.log("#crystal"+String(i)+": "+ $("#crystal"+String(i)).val())
+        }
+        $("#myscore").text(0);
+        console.log("new score is: " + myNumber);
     }
+
+    for (i = 1; i<5;i++){
+        $("#crystal"+String(i)).val(Math.floor(Math.random() * 12) + 1);
+        console.log("#crystal"+String(i)+": "+ $("#crystal"+String(i)).val())
+    }
+
+    $("#crystals").on("click", ".adder", function() {
+        
+        myNumber += parseInt($(this).val());
+        $("#myscore").html(myNumber);
+        if (myNumber === scoreToGuess){
+            console.log("final winning score is: " + myNumber);
+            wins += 1;
+            console.log("Wins: "+wins)
+            $("#wins").text("Wins: "+wins);
+            initializeGame();          
+        }
+        else if (myNumber > scoreToGuess){
+            console.log("final losing score is: " + myNumber);
+            loses += 1;
+            console.log("loses: "+loses)
+            $("#loses").text("loses: "+loses);
+            initializeGame();
+        }
+
+    });
+
+
+
+
 
 });
